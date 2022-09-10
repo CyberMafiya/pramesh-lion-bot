8a2780e510ae73eb178a43672b702cc28f5275b6
BLACKQUEENTEAM <109587968+BLACKQUEENTEAM@users.noreply.github.com>
2022-09-10 18:13:59 +0530

    Initial commit

---
+++ settings.js
@@ -0,0 +1,136 @@
+//☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒//
+//code by pramesh nilakshan//
+//මෙමෙ bot ඔබට ගොඩක් ප්‍රයෝජනවත් bot කෙනෙකි//
+//i am pramesh //
+//╳╳╳╳╳╳╳╳╳╳╳╳//
+//◈━━━━━━━━━━━━━◈//
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Website\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'your key',
+}
+
+
+//aumto functioner
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = false //auto reading in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
+
+//semt menu variants
+global.vidmenu = fs.readFileSync("./XeonMedia/theme/Cheems-bot.mp4") //gif and video menu
+
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.vcardowner = ['94704472592'] //⚙️⚙️
+global.ownername = "𝙥𝙧𝙖𝙢𝙚𝙨𝙝 𝙣𝙞𝙡𝙖𝙠𝙨𝙝𝙖𝙣" //එම අයිතිකරුගේ නම මෙතන්ට දාන්න
+//මේව වෙනස් කරන්න පුලුවන්😊
+global.owner = ['94704472592']
+global.ownertag = '94704472592' //මේ නම්බර් එකට ඔයාගෙ නම්බර එක දාගන්න කව්රුහරි මේකෙ masege  එකක් tag කරාම sticker එනව
+global.botscript = 'https://chat.whatsapp.com/E3enguWGKzLJT0PES2AhrY' //group link
+
+global.socialm = "GitHub: lion" //ur github or insta name
+global.location = "srilanka,colombo" //ur location
+// මෙතනින් යට මොනවත් වෙනස් කරන්න එපා 🚫⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️⬇️
+//වැඩ කරන්නෙ නැතුව යනව
+//☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒//
+
+global.linkz = "https://youtube.com/channel/UCOlt2_XK6tS2KJn6fTdpPew" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UCOlt2_XK6tS2KJn6fTdpPew" //ur website to be displayed
+global.reactmoji = "🦁" //ur menu react emoji code lock🔒
+global.themeemoji = "🦁" //ur theme emoji code lock 🔒
+prefix = "☞"//Menu comand star☞
+global.packname = "☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒" //ur sticker watermark packname code lock 🔒
+global.author = "☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒" //ur sticker watermark author code lock 🔒
+global.botname = '☬༒𝙥𝙧𝙖𝙢𝙚𝙨𝙝༆𝙡𝙞𝙤𝙣⃕ 𝙗𝙤𝙩༒' //code lock 🔒
+global.ytname = "pramesh lion bot tech" // code lock 🔒
+
+//Bot theme media
+global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+
+//damtabase
+global.premium = ['916909137213'] //ur premium numbers
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+//omther
+global.sessionName = 'session'
+global.antitags = true
+global.prefa = ['','!','.','🦁','⚙️','✅']
+global.sp = '⭔'
+global.mess = {
+    success: 'හරි ✅',
+    admin: 'මෙය කල හැක්කෙ grop  admin ට පමනයි💻',
+    botAdmin: 'මෙය කිරීමට මට admin අවශ්‍ය වේ ⚙️',
+    owner: 'මෙය කල හැක්කෙ මාගෙ අයිති කරුට පමනයි 🌚',
+    group: 'මෙම විදානයා පාවිච්චි කල හැක්කෙ grop වලදී පමනයි 👥👥',
+    private: 'මෙය කිරීම සදහා මාගෙ inbox පැමිනෙන්න',
+    bot: 'This Feature Is Only For Bot',
+    wait: ' මෙය සාදමින් පවතී  🛠️🛠️',
+    linkm: 'Where is the link?',
+    error: 'එහි දෝෂ පවතී 💥',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+    banChat: 'The bot was banned in this group, please contact the owner to unban'
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
